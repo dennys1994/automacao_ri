68270406bd79a9eb384c306044f71e82d4dc99e1
Dennys Silva <dennys_1994@live.com>
2025-11-10 14:36:16 -0300

    alterado caminho residenciais

--- modules/apagar_planilhas.py
+++ modules/apagar_planilhas.py
@@ -2,7 +2,7 @@
 import os
 
 def apagar_planilhas():
-    caminho_planilhas = 'RESIDENCIAIS'
+    caminho_planilhas = '../RESIDENCIAIS'
     for file in os.listdir(caminho_planilhas):
         file_delete = os.path.join(caminho_planilhas, file)
         os.remove(file_delete)
